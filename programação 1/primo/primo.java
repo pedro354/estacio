094a933073b2af39247ab58d58b944b2d2b5eb45
pedro354 <pedrosilva354@gmail.com>
2021-12-16 16:48:10 -0300

    l

---
+++ programação 1/primo/primo.java
@@ -0,0 +1,20 @@
+package primo;
+import java.util.Scanner;
+public class primo {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+    int nr, i, cont = 0;
+        System.out.print("Numero..: ");
+        nr = Integer.parseInt(sc.nextLine());
+        for (i=1; i<= nr; i++){
+            if (nr % i == 0) {
+                cont++;
+            }
+        }
+    if (cont == 2){
+        System.out.println("O numero é PRIMO");
+    } else {
+        System.out.println("O numero não é PRIMO");
+    }
+}
+}
