e85a07e8d6514eeeca469ad3193084eeaba7ae54
pedro354 <pedrosilva354@gmail.com>
2021-12-23 17:04:30 -0300

    a

---
+++ programação 1/orientaçãodeeobjetos/Aluno.java
@@ -0,0 +1,7 @@
+package orientaçãodeeobjetos;
+
+
+
+}
+
+}
---
+++ programação 1/orientaçãodeeobjetos/appAluno.java
@@ -0,0 +1,22 @@
+package orientaçãodeeobjetos;
+
+public class AppAluno {
+    public static void main (String[] args){
+        Aluno aluno1 = new Aluno();
+        Aluno aluno2 = new Aluno();
+        Aluno aluno3 = new Aluno();
+        aluno1.matricula = "1001";
+        aluno1.nome = "André";
+        aluno1.cr = 6.7;
+        aluno2.matricula = "1002";
+        aluno2.nome = "Maria";
+        aluno2.cr = 7.5;
+        aluno3.matricula = "1003";
+        aluno3.nome = "João";
+        aluno3.cr = 7.0;
+        aluno1.imprimir();
+        aluno2.imprimir();
+        aluno3.imprimir();
+    
+}
+
