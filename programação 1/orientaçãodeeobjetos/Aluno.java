556410be743265b4c9e4975515100fea61a30efd
pedro354 <pedrosilva354@gmail.com>
2021-12-28 09:41:42 -0300

    pronto

--- programação 1/orientaçãodeeobjetos/Aluno.java
+++ programação 1/orientaçãodeeobjetos/Aluno.java
@@ -1,7 +1,13 @@
 package orientaçãodeeobjetos;
 
+public class Aluno {
+    String matricula, nome;
+    double cr;
 
-
+    public void imprimir( ){
+        System.out.println("Matricula: " + matricula);
+        System.out.println("Nome: " + nome);
+        System.out.println("CR" + cr);
+    }
 }
 
-}
--- programação 1/orientaçãodeeobjetos/appAluno.java
+++ programação 1/orientaçãodeeobjetos/appAluno.java
@@ -19,4 +19,5 @@ public class AppAluno {
         aluno3.imprimir();
     
 }
+}
 
