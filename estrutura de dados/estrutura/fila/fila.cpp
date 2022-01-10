125362211160ce15c936d7f48a4498fd1b07efff
pedro354 <pedrosilva354@gmail.com>
2022-01-10 17:03:55 -0300

    fila

---
+++ estrutura de dados/estrutura/fila/fila.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <locale.h>
+#include <stdlib.h>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "PORTUGUESE");
+    
+}
